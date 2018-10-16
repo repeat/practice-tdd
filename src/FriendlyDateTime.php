316d168cebe87ad42400b7703fdb716c30330331
Repeat Yang <repeat@pixnet.tw>
2018-10-16 17:43:49 +0800

    add FriendlyDateTime class

---
+++ src/FriendlyDateTime.php
@@ -0,0 +1,28 @@
+<?php
+namespace Repeat\PracticeTDD;
+
+use Carbon\Carbon;
+
+/**
+ * FriendlyDateTime 友善的日期時間格式
+ */
+class FriendlyDateTime
+{
+    protected $timestamp;
+
+    public function __construct(int $timestamp)
+    {
+        $this->timestamp = $timestamp;
+    }
+
+    /**
+     * print 印出時間
+     *
+     * @return string 指定的時間格式
+     */
+    public function print()
+    {
+        $result = Carbon::createFromTimestamp($this->timestamp);
+        return $result->format('Y 年 n 月 j 日 G:i:s');
+    }
+}
