98db502e0333fceea0745565b5803c826d3ba2c6
repeat <repeat+gitcommit@gmail.com>
2018-10-16 23:10:22 +0800

    test: drop timezone support

--- tests/FriendlyDateTimeTest.php
+++ tests/FriendlyDateTimeTest.php
@@ -24,23 +24,4 @@ class FriendlyDateTimeTest extends TestCase
             [1535738584, '9 月 1 日 02:03'],
         ];
     }
-
-    /**
-     * @dataProvider dataPrintWithTimezone
-     */
-    public function testPrintWithTimezone($input, $expected)
-    {
-        $friendly = new FriendlyDateTime($input, 'UTC');
-        $actual = $friendly->print();
-        $this->assertSame($expected, $actual);
-    }
-
-    public function dataPrintWithTimezone()
-    {
-        return [
-            [1514735999, '2017 年 12 月 31 日 23:59'],
-            [1514736000, '1 月 1 日 00:00'],
-            [1535738584, '8 月 31 日 18:03'],
-        ];
-    }
 }
