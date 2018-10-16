9b55cb85fb3798a4261dce8aa909f9ba13c0567e
Repeat Yang <repeat@pixnet.tw>
2018-10-16 18:45:48 +0800

    remove date format :s for not display seconds

--- src/FriendlyDateTime.php
+++ src/FriendlyDateTime.php
@@ -23,6 +23,6 @@ class FriendlyDateTime
     public function print()
     {
         $result = Carbon::createFromTimestamp($this->timestamp);
-        return $result->format('Y 年 n 月 j 日 G:i:s');
+        return $result->format('Y 年 n 月 j 日 G:i');
     }
 }
