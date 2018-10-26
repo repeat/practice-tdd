c0cfddb5a12f8b58b82a74f1a8ac67e90153250c
Repeat Yang <repeat@pixnet.tw>
2018-10-26 18:45:01 +0800

    add logic for yesterday

--- src/FriendlyDateTime.php
+++ src/FriendlyDateTime.php
@@ -36,6 +36,9 @@ class FriendlyDateTime
         if ($from->diffInDays($to) < 1) {
             return $from->diffInHours($to) . ' 小時前';
         }
+        if ($from->diffInDays($to) < 2) {
+            return $from->format('昨天 H:i');
+        }
 
         if ($from->diffInSeconds($to) > (365 * 24 * 60 * 60)) {
             return $from->format('Y 年 n 月 j 日');
