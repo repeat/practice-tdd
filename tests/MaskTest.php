a76d95e59aa9ca923c6a578e81d0a301602928c4
Repeat Yang <repeat@pixnet.tw>
2020-02-05 15:35:20 +0800

    add tests for validate date range

---
+++ tests/MaskTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Repeat\PracticeTDD\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Carbon\Carbon;
+use Repeat\PracticeTDD\Mask;
+
+class MaskTest extends TestCase
+{
+    public function tearDown(): void
+    {
+        Carbon::setTestNow();
+    }
+
+    public function testIsValidDateRangeTrue(): void
+    {
+        Carbon::setTestNow(Carbon::parse('2020/02/06 0:00:00 Asia/Taipei'));
+        $this->assertTrue(Mask::isValidDateRange(Carbon::now()));
+    }
+
+    public function testIsValidDateRangeFalse(): void
+    {
+        Carbon::setTestNow(Carbon::parse('2020/02/05 23:59:59 Asia/Taipei'));
+        $this->assertFalse(Mask::isValidDateRange(Carbon::now()));
+    }
+}
