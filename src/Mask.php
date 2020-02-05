9df2ea91d4ab5a3140c827323000075a6a4b9721
Repeat Yang <repeat@pixnet.tw>
2020-02-05 15:48:09 +0800

    add const EFFECTIVE_DATE

--- src/Mask.php
+++ src/Mask.php
@@ -6,9 +6,11 @@ use Carbon\Carbon;
 
 class Mask
 {
+    const EFFECTIVE_DATE = '2020/02/06 0:00:00 Asia/Taipei';
+
     public static function isValidDateRange(Carbon $now): bool
     {
-        $effective_date = Carbon::parse('2020/02/06 0:00:00 Asia/Taipei');
+        $effective_date = Carbon::parse(self::EFFECTIVE_DATE);
         $now = Carbon::now();
 
         return $now->greaterThanOrEqualTo($effective_date);
