8ed0b8effe3133eb3faedce1147ec392d89fda4f
Repeat Yang <repeat@pixnet.tw>
2020-02-05 17:56:12 +0800

    simply test data

--- tests/MaskTest.php
+++ tests/MaskTest.php
@@ -15,13 +15,13 @@ class MaskTest extends TestCase
 
     public function testIsValidDateRangeTrue(): void
     {
-        Carbon::setTestNow(Carbon::parse('2020/02/06 0:00:00 Asia/Taipei'));
-        $this->assertTrue(Mask::isValidDateRange(Carbon::now()));
+        Carbon::setTestNow(Carbon::parse('2020/02/06 0:00:00'));
+        $this->assertTrue(Mask::isValidDateRange());
     }
 
     public function testIsValidDateRangeFalse(): void
     {
-        Carbon::setTestNow(Carbon::parse('2020/02/05 23:59:59 Asia/Taipei'));
-        $this->assertFalse(Mask::isValidDateRange(Carbon::now()));
+        Carbon::setTestNow(Carbon::parse('2020/02/05 23:59:59'));
+        $this->assertFalse(Mask::isValidDateRange());
     }
 }
