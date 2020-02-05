061e3d501855c53d933a68532814fa67c3243dfb
Repeat Yang <repeat@pixnet.tw>
2020-02-05 18:12:25 +0800

    add test data for final even residents

--- tests/MaskTest.php
+++ tests/MaskTest.php
@@ -24,4 +24,61 @@ class MaskTest extends TestCase
         Carbon::setTestNow(Carbon::parse('2020/02/05 23:59:59'));
         $this->assertFalse(Mask::isValidDateRange());
     }
+
+    /**
+     * @dataProvider dataIsValidEvenWeekdayTrue
+     */
+    public function testIsValidEvenWeekdayTrue(String $weekday, int $final): void
+    {
+        Carbon::setTestNow(Carbon::parse($weekday));
+        $this->assertTrue(Mask::isValidWeekday($final));
+    }
+
+    public function dataIsValidEvenWeekdayTrue(): array
+    {
+        $weekdays = [
+            'Tuesday',
+            'Thursday',
+            'Saturday',
+            'Sunday',
+        ];
+        $evens = [2, 4, 6, 8, 0];
+        $data = [];
+
+        foreach ($weekdays as $weekday) {
+            foreach ($evens as $even) {
+                $data[] = [$weekday, $even];
+            }
+        }
+
+        return $data;
+    }
+
+    /**
+     * @dataProvider dataIsValidEvenWeekdayFalse
+     */
+    public function testIsValidEvenWeekdayFalse(String $weekday, int $final): void
+    {
+        Carbon::setTestNow(Carbon::parse($weekday));
+        $this->assertFalse(Mask::isValidWeekday($final));
+    }
+
+    public function dataIsValidEvenWeekdayFalse(): array
+    {
+        $weekdays = [
+            'Monday',
+            'Wednesday',
+            'Friday',
+        ];
+        $evens = [2, 4, 6, 8, 0];
+        $data = [];
+
+        foreach ($weekdays as $weekday) {
+            foreach ($evens as $even) {
+                $data[] = [$weekday, $even];
+            }
+        }
+
+        return $data;
+    }
 }
